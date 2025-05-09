280d7f912e5f573b929fefdc195be74240920564
user <project1@nixos.iis.amadeus.net>
2025-05-09 19:37:23 +0000

    add

--- config.nix
+++ config.nix
@@ -1,6 +1,6 @@
 _: {
   users = {
-    nixos = {
+    iliyan = {
       initialHashedPassword = "$6$5GT0G5I0F1wAfwuu$Nt6BgjM45KYxrKJKzuMcgwgl/sE5eoem7q3tT386BVZMkUu/0iuqT.4vfj6o/TaTEOQEs.QbjEhSEgKyBNhYN0";
     };
     project1 = {
