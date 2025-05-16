604e5af5c58010565db86f62f006eacb4692889c
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2025-05-16 13:46:13 +0000

    fix: env vars
    M  hosts/system/env-vars/default.nix

--- hosts/system/env-vars/default.nix
+++ hosts/system/env-vars/default.nix
@@ -1,7 +1,12 @@
-{pkgs, ...}:
-pkgs.lib.recursiveUpdate {
-  NIX_LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.libapparmor}/lib";
-  PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-  MANPAGER = "nvim +Man!";
-  GTK_THEME = "Adwaita:dark";
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  environment.variables = {
+    NIX_LD_LIBRARY_PATH = lib.mkForce "${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.zlib}/lib:${pkgs.libapparmor}/lib";
+    PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    MANPAGER = "nvim +Man!";
+    GTK_THEME = "Adwaita:dark";
+  };
 }
