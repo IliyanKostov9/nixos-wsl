7f2bac129eb0728170866ea8a886ff325e95079f
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2025-05-30 08:07:48 +0000

    feat: added dbeaver
    A  programs/user/dbeaver/default.nix
    M  programs/user/default.nix
    D  programs/user/pcmanfm/default.nix
    M  programs/user/tmux/tmux.conf

---
+++ programs/user/dbeaver/default.nix
@@ -0,0 +1,15 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.modules.dbeaver;
+in {
+  options.modules.dbeaver = {enable = mkEnableOption "dbeaver";};
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [dbeaver-bin];
+  };
+}
--- programs/user/default.nix
+++ programs/user/default.nix
@@ -1,8 +1,8 @@
 _: {
   imports = [
     ./chromium
+    ./dbeaver
     ./firefox
-    ./pcmanfm
     ./librewolf
     ./fonts
     ./awscli
